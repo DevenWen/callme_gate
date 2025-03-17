1f38cc85dbe9caa310cb9131b1cb0e11bcc6357e
藤知 <wenkangqiang.wkq@alibaba-inc.com>
2025-03-18 01:54:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# 我的 Python 项目
+
+这是一个使用 Git 进行版本管理的基本 Python 项目。
+
+## 功能
+
+- 基本的问候功能
+- 简单的数学运算
+
+## 如何使用
+
+1. 克隆仓库
+2. 运行 `python main.py`
+
+## 测试
+
+运行 `python tests/test_utils.py` 执行测试 
---
+++ build.sh
@@ -0,0 +1,18 @@
+# 
+rm -rf tabulate
+rm -rf tabulate-.*
+
+pip install -t . tabulate
+
+# 获取当前时间戳
+timestamp=$(date +%Y%m%d%H%M%S)
+# 获取git最近一次提交的前7位字符
+commit_hash=$(git rev-parse --short HEAD)
+
+zip_filename="build/code-${timestamp}-${commit_hash}.zip"
+zip $zip_filename -r ./*
+
+# 清理文件
+rm -rf tabulate
+rm -rf tabulate-.*
+
